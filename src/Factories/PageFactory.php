305578dc38bf83df3e71a6dd02cf5ec74f9c705a
Diogo Santo <legiaifenix@gmail.com>
2020-05-11 09:53:47 +0100

    Adding PageFactory

---
+++ src/Factories/PageFactory.php
@@ -0,0 +1,34 @@
+<?php
+namespace legiaifenix\wpcftoop\Factories;
+
+
+class PageFactory implements Factory
+{
+    /**
+     * @var string $filePath                As the path to a JSON file that would loop generate out CFT
+     */
+    protected $filePath;
+
+    /**
+     * @var string $name                    As the name of the CFT we want to generate, if we only desire one
+     */
+    protected $name;
+
+    public function setFilePath(string $filePath)
+    {
+        $this->filePath = $filePath;
+        return $this;
+    }
+
+    public function setName(string $name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    public function create()
+    {
+        // TODO: Implement create() method.
+    }
+
+}
